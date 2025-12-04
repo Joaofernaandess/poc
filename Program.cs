bbd2d50dc59e1f46406016dbdef91dbd95903c6a
Joaofernaandess <jf030626@gmail.com>
2025-12-04 17:24:28 -0300

    status 04-12

--- Program.cs
+++ Program.cs
@@ -1,3 +1,51 @@
+using System.Runtime.CompilerServices;
 using GestãoApi;
 var builder = WebApplication.CreateBuilder(args);
-builder = app
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ClienteRepository>();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.MapPost("/Cliente", async (Cliente cliente, ClienteRepository repo) =>
+{
+    if(string.IsNullOrEmpty(cliente.Nome))
+    {
+        return Results.BadRequest("Ei! O nome é obrigatório.");
+    }
+    await repo.CreatAsync(cliente);
+    return Results.Created($"/clientes/{cliente.Clienteid}", cliente);
+})
+.WithOpenApi();
+
+app.MapGet("/cliente", Async(ClienteRepository repo) =>
+{
+    var lista = await repo.GetAllAsync();
+    return Results.Ok(lista);
+})
+
+.WithOpenApi();
+
+app.MapGet("/clientes/{id}", async (Guid id, ClienteRepository repo) =>
+{
+    var cliente = await GetBIdAsnc(id);
+    if (cliente == null) return Results.NotFound("Cliente não existe!");
+
+    return Results.Ok(cliente);
+})
+
+.WithOpenApi();
+
+app.MapDelete("/cliente/{id}", async (Guid id, ClienteRepository repo) =>
+{
+    await repo.DeleteAsync(id);
+    return Results.ok("Cliente removido com sucesso!");
+})
+.WithOpenApi();
+app.run();
